c92fdb21a88b0bf7121c3cbc882a2ba90607cc39
Archisman Bhaumik <archismanbhaumik786@gmail.com>
2023-02-05 21:03:25 +0530

    comments added

--- server.js
+++ server.js
@@ -4,22 +4,28 @@ const bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
+
+//functions which operates on given string input
 const functions = {
+  //returns the uniquecount of any element 
   uniqueCount: (...args) => {
     return new Set(args).size;
   },
   ksmallest: (k,...args) => {
+    //returns the kth smallest lexicographic order
     console.log(k);
     return [parseInt(k)-args.sort() ];
     
   },
   pickRandom: (...args) => {
+    //picks any random element from the array
     return args[Math.floor(Math.random() * args.length)];
   }
 };
 
-
+//string parser
 const evaluateExpression = (expression) => {
+  //pointers to move through the string input
   let currentIndex = 0;
   const nextChar = () => expression[currentIndex++];
   const peekChar = () => expression[currentIndex];
@@ -55,6 +61,7 @@ const evaluateExpression = (expression) => {
       }
     }
     nextChar();
+    //if the function is not able to parse the given input
     if (!functions[functionName]) {
       throw new Error(`Function ${functionName} not supported`);
     }
@@ -64,6 +71,7 @@ const evaluateExpression = (expression) => {
   return parseExpression();
 };
 
+//the post method which passes our given string input
 app.post('/evaluate', (req, res) => {
   try {
     const result = evaluateExpression(req.body.expression);
@@ -73,6 +81,7 @@ app.post('/evaluate', (req, res) => {
   }
 });
 
+//defining our port
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Expression evaluator API listening on port ${port}`);
